397dc8e42c95e2d03587fd45b848dbdb1d481cd9
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-22 14:40:29 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char ya, yb, yc;
+    cin >> ya >> a;
+    cin >> yb >> b;
+    cin >> yc >> c;
+
+    if ( (ya="Y" && a>=37) ) {
+        if ( (yb="Y" && b>=37) || (yc="Y" && c>=37))
+            cout << "E";
+        else
+            cout << "N";
+    }
+    else {
+        if ( (yb="Y" && b>=37) && (yc="Y" && c>=37) )
+            cout << "E";
+        else
+            cout << "N";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
