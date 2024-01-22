0bb46294d61768e8318e923ae83842b726fa691a
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-22 15:50:41 +0900

    Title: b부터 a까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+    cin >> a >> b;
+
+    for (i=b; i>=a; i--)
+        cout << i << " ";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
