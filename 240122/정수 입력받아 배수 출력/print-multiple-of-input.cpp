44546ee4ded47273c97e712b31fd0d8bbc718828
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-22 15:39:41 +0900

    Title: 정수 입력받아 배수 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    cin >> n;
+
+    for (i=1;i<=5;i+=1)
+        cout << n*i << " ";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
