af1248cf138a86ec4f8bd843b3846832d019d82c
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-23 10:23:55 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+    cin >> a >> b;
+
+    if (a>=b) {
+        for (i=a;i>=b;i--)
+            cout << i << " "; 
+    }
+    else if (b>a) {
+        for (i=b;i>=a;i--)
+            cout << i << " "; 
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
