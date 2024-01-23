c42e9a993024cd7aba411fc1943d5bb9031d60ad
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-23 13:46:37 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,n,cnt;
+    cnt = 0;
+    i=1;
+
+    while (i<=5) {
+        cin >> n;
+        if (n%2==0)
+        cnt++;
+        i++;
+    }
+    cout << cnt;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
