71c1c68e82f94660a8020fda0581f1656928759e
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-23 11:18:33 +0900

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/369 게임/369-game.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    cin >> n;
+
+    i=1;
+
+    while (i<=n) {
+        if (i%3==0)
+            cout << 0 << " ";
+        else if (i>=10) {
+            if ((i%((i/10)*10))%3==0)
+                cout << 0 << " ";
+            else 
+                cout << i << " ";
+        }
+        else
+            cout << i << " ";
+        i+=1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
