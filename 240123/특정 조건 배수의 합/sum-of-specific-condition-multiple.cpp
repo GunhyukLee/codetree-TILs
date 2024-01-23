9a98ec0477eb9cb31781d5df52984f4055d6fe43
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-23 14:21:45 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240123/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240123/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -5,12 +5,21 @@ int main() {
     int a,b,i,sum;
     cin >> a >> b;
     sum = 0;
-    for (i=a; i<=b; i++) {
-        if (i%5==0) {
-            sum+=i;
-        }
-    }
 
+    if (b>=a) {
+        for (i=a; i<=b; i++) {
+          if (i%5==0) {
+              sum+=i;
+            }
+       }
+    }
+    else {
+        for (i=b; i<=a; i++) {
+          if (i%5==0) {
+              sum+=i;
+            }
+       }
+    }
     cout << sum;
     // 여기에 코드를 작성해주세요.
     return 0;
