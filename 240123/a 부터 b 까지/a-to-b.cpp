fe42c13e957430942ea1fe0d3886a16132f87cf1
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-23 10:54:07 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,i;
+    cin >> a >> b;
+    
+    for (i=1; i<=b; i) {
+        if (i%2==1) {
+            i= i*2;
+            if (a<=i && i<=b) {
+                cout << i << " ";
+            }
+        }
+        else if (i%2==0) {
+            i= i+3;
+            if (a<=i && i<=b) {
+                cout << i << " ";
+            }
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
