247f72c00a6b60d1f2e0587fb8c7c2e274ee52eb
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-29 18:18:28 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/ab 사이에 없는 c/c-between-a-and-b-2.cpp
+++ 240129/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main() {
     int a,b,c;
-    bool d = false;
+    bool d = true;
     cin >> a >> b >> c;
 
     for (int i=a;i<=b;i++) {
         if (i%c==0) {
-            d = true;
+            d = false;
         }
     }
     if (d==true) {
-        cout << "NO";
+        cout << "YES";
     }
     else {
-        cout << "YES";
+        cout << "NO";
     }
     
     // 여기에 코드를 작성해주세요.
