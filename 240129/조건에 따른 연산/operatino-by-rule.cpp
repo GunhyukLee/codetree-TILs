c5d82e80ccb3658e81d0e83b447d0a22599adfa7
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-29 17:39:38 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/조건에 따른 연산/operatino-by-rule.cpp
+++ 240129/조건에 따른 연산/operatino-by-rule.cpp
@@ -7,20 +7,17 @@ int main() {
     cin >> n;
 
     while (1) {
+        if (n>=1000) {
+            cout << cnt;
+            break;
+        } 
         if (n%2==0) {
             n=n*3+1;
         }
         else {
             n=n*2+2;
         }
-        
         cnt++;
-        
-        if (n>=1000) {
-            cout << cnt;
-            break;
-        } 
-
     }
     // 여기에 코드를 작성해주세요.
     return 0;
