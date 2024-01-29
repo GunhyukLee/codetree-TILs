b85e78016b94dd51ed0218238a3bdd120aea9ccc
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-29 15:26:57 +0900

    Title: 20대의 평균나이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/20대의 평균나이/average-age-of-20.cpp
+++ 240129/20대의 평균나이/average-age-of-20.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main() {
     int a;
     int sum = 0;
-    int n = 1;
+    int n = 0;
+    int i=1;
 
     while (1) {
         cin >> a;
-        
-        if ((a<20) || (30<a)) {
+
+        if (a<20 || 30<=a) {
             cout << fixed; cout.precision(2);
             cout << (double)sum/n;
             break;
         }
-        sum += a;
-        n += 1;
+        sum +=a;
+        n +=1;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
