7a8333c2a1243031cca7fbc2195938fc46968ae7
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-01-29 12:19:06 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    while (true) {
+        cin >> a;
+        if (a==0) {
+            break;
+        }
+        cout << a << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
