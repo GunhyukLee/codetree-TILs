7194317b1fc962cba3c1708e5a41ad1df43c7b88
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-01 18:07:21 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        if (i%2==1) {
+            for (int j=1; j<=n-i/2; j++) {
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=1;j<=i/2;j++) {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i=n; i>=1; i--) {
+        if (i%2==1) {
+            for (int j=1; j<=n-i/2; j++) {
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=1;j<=i/2;j++) {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
