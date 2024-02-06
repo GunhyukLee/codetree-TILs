47f9168d51cb470f90723ff7249bd959af3bfc64
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-06 11:27:58 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240206/별표 출력하기 12/print-star-12.cpp
+++ 240206/별표 출력하기 12/print-star-12.cpp
@@ -7,8 +7,11 @@ int main() {
 
     for (int i=1; i<=n; i++) {
         for (int j=1; j<=n;j++) {
-            if j==1 || j==n || (j%2==0 && i>=j)) {
+            if (i==1 || j==n || (j>=i && j%2==0)) {
                 cout << "* ";
+            }
+            else {
+                cout << "  ";
             }
         }
         cout << endl;
