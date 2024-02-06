27189a8d216b900faf0b6732664e5b5c3510e758
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-06 11:28:44 +0900

    Title: 별표 출력하기 12, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/별표 출력하기 12/print-star-12.cpp
+++ 240206/별표 출력하기 12/print-star-12.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for (int i=1; i<=n; i++) {
         for (int j=1; j<=n;j++) {
-            if (i==1 || j==n || (j>=i && j%2==0)) {
+            if (i==1 || (j>=i && j%2==0)) {
                 cout << "* ";
             }
             else {
