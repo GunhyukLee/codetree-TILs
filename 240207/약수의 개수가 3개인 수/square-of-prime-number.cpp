4c868f44fd0d3f1cd562f0c561927f381596801f
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-07 16:25:43 +0900

    Title: 약수의 개수가 3개인 수, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240207/약수의 개수가 3개인 수/square-of-prime-number.cpp
+++ 240207/약수의 개수가 3개인 수/square-of-prime-number.cpp
@@ -8,7 +8,7 @@ int main() {
     int cntt= 0;
     for (int i=start; i<=end; i++) {
         int cnt = 0;
-        for (int j=1;j<=i-1;j++) {
+        for (int j=1;j<=i;j++) {
             if (i%j==0) {
                 cnt++;
             }
