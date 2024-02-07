d72b425e34edbfd9b2fb536f283d4007a814a31b
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-07 12:13:16 +0900

    Title: 알파벳 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240207/알파벳 출력하기/print-alphabet.cpp
+++ 240207/알파벳 출력하기/print-alphabet.cpp
@@ -10,6 +10,9 @@ int main() {
             cout << char(x);
             x++;
         }
+        if (char(x)=="Z") {
+            x=(int)'A'
+        }
         for (int j=1; j<=n-i; j++) {
             cout << " ";
         }
