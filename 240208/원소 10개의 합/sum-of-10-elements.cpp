46d87d2a7582543a1ee4994508ef383054204b14
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-08 14:25:26 +0900

    Title: 원소 10개의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int sum = 0;
+    
+    for (int i=0; i<10; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
