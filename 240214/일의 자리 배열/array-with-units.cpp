d5eec897608d48d7d76984c71802ec9676e0bd80
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-14 12:31:14 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+    int b,c;
+    cin >> b >> c;
+
+    a[0]=b;
+    a[1]=c;
+
+    for (int i=2;i<10;i++) {
+        a[i] = (a[i-1]+a[i-2])%10;
+    }
+
+    for (int i=0;i<10;i++) {
+        cout << a[i] << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
