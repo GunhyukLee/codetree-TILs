dbb1b41a836fb0f96b1ef02064d16582ba0a433d
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-02-15 12:24:35 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/코로나 메뉴얼2/covid-manual2.cpp
+++ 240215/코로나 메뉴얼2/covid-manual2.cpp
@@ -29,11 +29,10 @@ int main() {
 
     for (int i=0;i<4;i++) {
         cout << cnt_B[i] << " ";
-        if (cnt_B[0]>=2) {
-            cout << "E";
-        }
     }
-
+    if (cnt_B[0]>=2) {
+        cout << "E";
+    }
     // 여기에 코드를 작성해주세요.
     return 0;
 }
