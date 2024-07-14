551bcda464729e0b6741130268987704c7ebed6f
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 06:20:27 +0900

    Title: 차례로 출력, Time: 4ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/차례로 출력/print-in-order.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    a = 5;
+    b = 17;
+
+    for (int i=a;i<=b;i++) {
+        cout << i << " ";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
