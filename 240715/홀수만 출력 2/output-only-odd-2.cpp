bb1c57a12119a7c95f4dbfd7c5a5d3f6cafd3399
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 06:42:41 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/홀수만 출력 2/output-only-odd-2.cpp
+++ 240715/홀수만 출력 2/output-only-odd-2.cpp
@@ -5,8 +5,12 @@ int main() {
     int a,b,i;
     cin >> b >> a;
 
-    for (i=b; i>=a; i-=2)
-        cout << i << " ";
+    for (i=b; i>=a; i--) {
+        if (i%2==1) {
+            cout << i << " ";
+        }
+    }
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
