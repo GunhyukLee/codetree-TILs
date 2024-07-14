bd069b33a5525778777c94c2761689b5458529fe
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 07:09:31 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/n번 더하기/add-n-times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n;
+    cin >> a >> n;
+
+    int i= 1;
+
+    while (i<=n) {
+        a+=n;
+        cout << a << endl;
+        i++;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
