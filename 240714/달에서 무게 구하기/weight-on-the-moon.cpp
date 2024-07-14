7ad2dca4ec9c269edb29dcae470a3a1826ad1001
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-14 23:56:35 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 13;
+    double g = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << a << " * " << g << " = " << a*g; 
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
