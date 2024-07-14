29f37d27c5e2caf4a96d287b2e1cf834631f7e06
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 06:27:34 +0900

    Title: 증가시키며 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    for (i=5;i<=17;i+=2) {
+        cout << i << " ";
+    }
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
