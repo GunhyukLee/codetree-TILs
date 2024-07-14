0be0c0e270d5b1fb07012dcc8af9663da05710ef
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 04:26:04 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    int z = 10;
+
+    cin >> z;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+
+    cout << 0 << z << "-" << y << "-" << x;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
