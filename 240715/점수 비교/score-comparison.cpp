73cbd85bb3256185278716599984d24e3c2b1617
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 05:22:30 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/점수 비교/score-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_m, A_e, B_m, B_e;
+    cin >> A_m >> A_e;
+    cin >> B_m >> B_e;
+
+    cout << (A_m>B_m && A_e>B_e); 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
