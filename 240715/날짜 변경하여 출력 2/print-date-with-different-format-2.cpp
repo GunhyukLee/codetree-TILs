22c024742f82d8bc829bd0a8b97df73f9c41ca0c
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 03:24:43 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int mm, dd , yyyy;
+    char d;
+    
+    cin >> mm >> d >> dd >> d >> yyyy;
+
+    cout << yyyy << "." << mm << "." << dd;
+
+	return 0;
+
+}
