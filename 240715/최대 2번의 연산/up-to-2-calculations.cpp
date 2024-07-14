10008d5fe8c2a2311249fee86520be6ceebda435
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 04:59:48 +0900

    Title: 최대 2번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a%2==0)
+        a /= 2;
+    if (a%2==1) {
+        a += 1;
+        a /= 2;
+    }
+    cout << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
