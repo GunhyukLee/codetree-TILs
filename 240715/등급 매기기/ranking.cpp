59daea4fb847399f525edd3ec19c2edc34545f02
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-15 04:56:29 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/등급 매기기/ranking.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+    scanf("%d",&score);
+
+    if (score>=90) {
+        cout << "A";
+    }
+    else if (score>=80) {
+        cout << "B";
+    }
+    else if (score>=70) {
+        cout << "C";
+    }
+    else if (score>=60) {
+        cout << "D";
+    }
+    else {
+        cout << "F";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
