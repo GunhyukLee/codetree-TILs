c14bec88b526c51f4dd2aeaeff192d9d7ddb07f6
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-17 03:43:33 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+    int sum1 = 0;
+    int cnt1 = 0;
+    int sum2 = 0;
+    int cnt2 = 0;
+
+    for (int i=0;i<10;i++) {
+        cin >> a[i];
+    }
+
+    for (int j=1;j<10;j+=2) {
+        sum1 += a[j];
+        cnt1 ++;
+    }
+
+    for (int k=2;k<10;k+=3) {
+        sum2 += a[k];
+        cnt2 ++;
+    }
+
+    cout << fixed, cout.precision(1);
+    cout << sum1 << " " << (double)sum2/cnt2;
+    
+    return 0;
+}
