1e81b176adef52760bca4299199679f18e6e4cc8
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-17 03:42:12 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+
+    for (int i=0;i<10;i++) {
+        cin >> a[i];
+    }
+    cout << a[2]+a[4]+a[9];
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
