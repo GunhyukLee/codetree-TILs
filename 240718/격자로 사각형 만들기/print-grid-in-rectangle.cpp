8b5ebe9b525e8006a4875e8184948a7f1d08492d
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-18 06:02:05 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ 240718/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -13,14 +13,14 @@ int main() {
         arr[i][0] = 1;
     }
 
-    for (int i=1;i<5;i++) {
-        for (int j=1;j<5;j++) {
+    for (int i=1;i<n;i++) {
+        for (int j=1;j<n;j++) {
             arr[i][j] = arr[i-1][j] + arr[i][j-1] + arr[i-1][j-1];
         }
     }
 
-    for (int i=0;i<5;i++) {
-        for (int j=0;j<5;j++) {
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
