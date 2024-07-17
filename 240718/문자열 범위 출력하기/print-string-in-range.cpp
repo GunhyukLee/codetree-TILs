905c958847c2bd237a39217b93d48f5fc6eddc02
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-18 06:38:07 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/문자열 범위 출력하기/print-string-in-range.cpp
+++ 240718/문자열 범위 출력하기/print-string-in-range.cpp
@@ -6,23 +6,10 @@ int main() {
     string str;
     getline(cin,str);
 
-    bool A = false;
 
-    int i = 1;
-    while(i<=str.length()) {
-        if (str[i] ==' ') {
-            A = true;
-        }
-        i++;
-    }
-    if (A==true) {
         for (int i=2;i<10;i++) {
             cout << str[i];
         }
-    }
-
     
-
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
