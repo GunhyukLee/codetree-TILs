e1827c6e7fe79684a9f769047d8b91b6b65120a6
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-18 23:45:49 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/부분문자열 위치 구하기/find-location-of-substring.cpp
+++ 240718/부분문자열 위치 구하기/find-location-of-substring.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+string input_str, target_str;
+
 int main() {
-    string A, B;
-    cin >> A >> B;
-    int cnt =0;
-    int count=0;
+    cin >> input_str >> target_str;
 
-    bool C = false;
-    int d = 0;
-    for (int i=0;i<A.length()-B.length()+1;i++) {
-    
-        for (int j=0;j<B.length();j++) {
-            if (A[i+j]==B[j]) {
-                count++;
-            }
-            else {
-                count = 0;
-            }
+    int input_len = input_str.size();
+    int target_len = target_str.size();
 
-            if (count==B.length()) {
-                C = true;
-                d = i;
-            }
-        } 
+    // 입력 문자열의 각 문자를 시작점으로 하여 목적 문자열을 만들 수 있는지 확인합니다.
+    for(int i = 0; i < input_len; i++) {
+        // input_str의 i 부터 i + target_len - 1까지의 원소가
+        // target_len의 0부터 target_len - 1 까지의 원소와
+		// 정확히 일치하는지 확인합니다.
+        
+        // 만약 input_str의 끝 원소인 i + target_len - 1 번째가
+		// 존재하지 않는다면 비교를 하지 않습니다.
+        if(i + target_len - 1 >= input_len)
+            continue;
+        
+        // input_str의 s_idx1에서 e_idx1 까지의 문자열과
+        // output_str의 s_idx2에서 e_idx2 까지의 문자열과 일치하는지를 비교합니다.
+        bool is_matched = true;
+        for(int j = 0; j < target_len; j++) {
+            if(input_str[i + j] != target_str[j])
+                is_matched = false;
+        }
+        
+        if(is_matched) {
+            // 모든 문자에 대하여 매칭이 된 경우:
+            cout << i;
+            return 0;
+        }
     }
-    if (C==true) 
-    cout << d;
-    else
+
+    // 매칭이 되지 않는 경우:
     cout << -1;
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
