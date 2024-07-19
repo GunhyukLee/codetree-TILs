74336d369e563104fed64ae2eeaebfdbe9b51217
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-20 00:14:53 +0900

    Title: 국영수 순이지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Score {
+    public:
+        string name;
+        int k, e, m;
+    Score(string name="",int k=0, int e=0, int m=0) {
+        this->name=name;
+        this->k=k;
+        this->e=e;
+        this->m=m;
+    }
+};
+
+bool com(Score a, Score b) {
+    if (a.e==b.e) {
+        return a.m>b.m;
+    }
+    if (a.k==b.k) {
+        return a.e > b.e;
+    }
+    return a.k > b.k;
+}
+int main() {
+    int n;
+    cin >> n;
+
+    Score mem[n];
+
+    for (int i=0;i<n;i++) {
+        string name;
+        int k, e, m;
+        cin >> name >> k >> e >> m;
+        mem[i] = Score(name,k,e,m);
+    }
+
+    sort(mem,mem+n,com);
+
+    for (int i=0;i<n;i++) {
+        cout << mem[i].name << " " << mem[i].k << " " << mem[i].e << " " << mem[i].m << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
