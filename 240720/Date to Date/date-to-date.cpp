0997d81a345ee7ef17e5cace50c6221fb4f98864
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-20 00:59:30 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240720/Date to Date/date-to-date.cpp
+++ 240720/Date to Date/date-to-date.cpp
@@ -5,7 +5,7 @@ int main() {
     int m1,d1,m2,d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int day[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 
     int day_s = d1;
     int month_s = m1;
@@ -18,7 +18,7 @@ int main() {
         cnt++;
         day_s++;
         
-        if (day_s > month[m1]) {
+        if (day_s > day[m1]) {
             month_s++;
             day_s=1;
         }
