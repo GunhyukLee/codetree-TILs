949961bd06bc3e070f013420aa4611cb4a310031
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-19 10:57:54 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void A(int n, int m) {
+    int min;
+    if (n>=m) {
+        min = m;
+    }
+    else {
+        min = n;
+    }
+    int max = 1;
+    for (int i=1;i<=min;i++) {
+        if (n%i==0 && m%i==0) {
+            max = i;
+        }     
+    }
+    cout << max;
+}
+
+int main() {
+    int n ,m;
+    cin >> n >> m;
+    A(n,m);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
