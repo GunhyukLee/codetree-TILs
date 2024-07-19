84481ee5e72569e810a5c1b923509bf1ee620397
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-20 04:17:30 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240720/만나는 그 순간/the-moment-we-meet.cpp
+++ 240720/만나는 그 순간/the-moment-we-meet.cpp
@@ -1,54 +1,60 @@
 #include <iostream>
+
+#define MAX_T 1000000
+
 using namespace std;
 
+int n, m;
+int pos_a[MAX_T  + 1], pos_b[MAX_T + 1];
+
 int main() {
-    int N, M;
-    cin >> N >> M;
-
-    int A[10000]={};
-    int B[10000]={};
-
+    // 입력
+    cin >> n >> m;
+    
+    // A가 매 초마다 서있는 위치를 기록
     int time_a = 1;
-    for (int i=0;i<N;i++) {
-        char D;
-        int m;
-        cin >> D >> m;
-
-        for (int j=1;j<=m;j++) {
-            if (D=='L') {
-                A[time_a] = A[time_a-1] - 1;
-            }
-            else {
-                A[time_a] = A[time_a-1] + 1;
-            }
+    for(int i = 0; i < n; i++) {
+        char d; int t;
+        cin >> d >> t;
+        
+        while(t--) {
+            if(d == 'R')
+                pos_a[time_a] = pos_a[time_a - 1] + 1;
+            else
+                pos_a[time_a] = pos_a[time_a - 1] - 1;
+            
             time_a++;
         }
     }
-
+    
+    // B가 매 초마다 서있는 위치를 기록
     int time_b = 1;
-    for (int i=0;i<M;i++) {
-        char D;
-        int m;
-        cin >> D >> m;
-
-        for (int j=1;j<=m;j++) {
-            if (D=='L') {
-                B[time_b] = B[time_b-1] - 1;
-            }
-            else {
-                B[time_b] = B[time_b-1] + 1;
-            }
+    for(int i = 0; i < m; i++) {
+        char d; int t;
+        cin >> d >> t;
+        
+        while(t--) {
+            if(d == 'R')
+                pos_b[time_b] = pos_b[time_b - 1] + 1;
+            else
+                pos_b[time_b] = pos_b[time_b - 1] - 1;
+            
             time_b++;
         }
     }
-
-    int cnt=-1;
-    for (int i=1;i<1000;i++) {
-        if (A[i]==B[i]) {
-            cnt = i;
+    
+    // 최초로 만나는 시간을 구합니다.
+    int ans = -1;
+    for(int i = 1; i < time_a; i++) {
+        if(pos_a[i] == pos_b[i]) {
+            ans = i;
             break;
         }
     }
+    
+    cout << ans;
+    return 0;
+}
     cout << cnt;
     // 여기에 코드를 작성해주세요.
     return 0;
