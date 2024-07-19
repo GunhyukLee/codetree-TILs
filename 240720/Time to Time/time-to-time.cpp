7c63d35f0c07df104cc3d51162fefd4f056868bd
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-20 00:30:37 +0900

    Title: Time to Time, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/Time to Time/time-to-time.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << (c*60+d)-(a*60+b);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
