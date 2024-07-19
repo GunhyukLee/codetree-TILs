45b56042b3505d23a995f0f007ed5513e8b35b18
GEONHYUK LEE <87965486+GunhyukLee@users.noreply.github.com>
2024-07-19 21:03:02 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/단어 정렬/sorting-words.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string str[n];
+
+    for (int i=0; i<n; i++) {
+        cin >> str[i];
+    }
+
+    sort(str,str+n);
+    for (int i=0; i<n; i++) {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
